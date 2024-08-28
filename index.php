d6896495fe288bad6c9a0a81f1173007befaa7fe
Zapip <zafif3165@gmail.com>
2024-08-28 17:41:39 +0700

    Pertemuan 3

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
-
 include "header.php";
-
-#include "body.php";
-#include "newslettter.php";
-#include "footer.php";
-?> 
+include "body.php";
+include "newsletter.php";
+include "footer.php";
+?>
